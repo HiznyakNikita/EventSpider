b2ed85c9e52676df16bf70141f0ffebde50c9c5f
HiznyakNikita <hiznyaknikita1996@gmail.com>
2016-03-14 23:43:32 +0200

    add helper methods for work with arrays

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
 var http = require("http");
-
 var iconv = require('iconv-lite');
 
-function download(url, callback) {
+var knownPages = [];
+var visitedPages = [];
+
+//helper methods
+
+var isExistsInArray = function(arr,val){
+	for(var i=0;i<arr.length;i++) 
+		if(arr[i]==val) 
+			return true;
+    return false;
+}
+
+var pushNewItemsToArray = function(arr) {
+    var a = [];
+    var l = arr.length;
+    for(var i=0; i<l; i++) {
+      for(var j=i+1; j<l; j++) {
+        if (arr[i] === arr[j])
+          j = ++i;
+      }
+      a.push(arr[i]);
+    }
+    return a;
+};
+
+//server methods
+
+var downloadPage = function(url, callback) {
   http.get(url, function(res) {
     var data = "";
 	res.setEncoding('binary');
@@ -19,10 +45,10 @@ function download(url, callback) {
 
 var url = "http://today.kiev.ua"
 
-download(url, function(data) {
+downloadPage(url, function(data) {
   if (data) {
 	  data = iconv.decode(data, 'win1251');
-       console.log(data);
+       
   }
   else console.log("error");  
 });
